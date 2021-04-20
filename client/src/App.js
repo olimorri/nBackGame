bcc2b3060310f6353eae5018824ebc15530010a0
Oliver Morrison <olivercharlesmorrison@gmail.com>
2021-04-20 20:51:06 +0100

    feat: added routing and included passing of props within the route

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import GameBoard from './gameBoard';
+import Landing from './landing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Landing} exact />
+            <Route path="/game/:name" render={(props) => <GameBoard {...props} />} />
+          </Switch>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
